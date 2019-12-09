44d61bea36e173abc3c81517189d0999c6755097
Miley Wright <dishaliekwright@yahoo.com>
2019-12-09 07:17:41 -0500

    completed the first two display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Display from '../display/Display';
-import * as rtl from 'react-testing-library';
+import {render} from '@testing-library/react';
 
 test('Defaults to unlocked and open', () => {
-    const wrapper = rtl.render(<Display />);
+    const wrapper = render(<Display />);
     const unlocked = wrapper.getByText(/unlocked/i);
     const open = wrapper.getByText(/open/i);
 
     expect(unlocked.textContent).toMatch('Unlocked');
     expect(open.textContent).toMatch('Open');
+})
+
+test('Displays if gate is closed and locked', () =>{
+    const {getByText} = render(<Display locked={true} closed={true} />)
+    const locked = getByText(/locked/i);
+    const closed = getByText(/closed/i);
+
+    expect(locked.textContent).toMatch('Locked');
+    expect(closed.textContent).toMatch('Closed');
 })
